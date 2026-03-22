41af082c23524ca9c8942c8e86dc50ad64794e03
4rmy <hjacob215@gmail.com>
2026-03-22 18:19:48 -0400

    Chassis object

--- include/VexLibrary/Lib.hpp
+++ include/VexLibrary/Lib.hpp
@@ -5,15 +5,20 @@ namespace Lib {
 // constants
 static constexpr int DELAY = 10;
 
-bool init();
-bool opcontrol();
-bool auton();
+void init();
+void opcontrol();
+void auton();
 
 } // namespace Lib
 
 // utilities
-#include "utils/pose.hpp" // position + heading
+#include "utils/pose.hpp"           // position + heading
 #include "utils/tracking_wheel.hpp" // tracking wheels
 
 // sub systems
 #include "subs/pid.hpp" // PID controller
+
+// library callbacks to sync with VEX
+inline void Lib::init() {}
+inline void Lib::opcontrol() {}
+inline void Lib::auton() {}
--- include/VexLibrary/utils/chassis.hpp
+++ include/VexLibrary/utils/chassis.hpp
@@ -1,3 +1,28 @@
+#include "VexLibrary/Lib.hpp"
+#include "pros/imu.hpp"
+#include "pros/motor_group.hpp"
+#include <vector>
+
 namespace Lib {
-class Chassis {};
+class Chassis {
+protected:
+  pros::MotorGroup *left_motors;
+  pros::MotorGroup *right_motors;
+  pros::IMU *imu;
+  std::vector<TrackingWheel> tracking_wheels;
+  float track_width;
+
+public:
+  struct settings {
+    pros::MotorGroup *left_motors;
+    pros::MotorGroup *right_motors;
+    pros::IMU *imu;
+    std::vector<TrackingWheel> tracking_wheels = {};
+    float track_width = 0; // don't need if you have an IMU or 2 horizontal tracking wheels,
+                           // only used to calculate heading
+  };
+  Chassis(settings s)
+      : left_motors(s.left_motors), right_motors(s.right_motors), imu(s.imu),
+        tracking_wheels(s.tracking_wheels), track_width(s.track_width) {}
+};
 } // namespace Lib
